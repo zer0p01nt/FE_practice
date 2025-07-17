91fc2e8f310c06760c2ff73d751d2d389764ef89
zer0p01nt <1027.bmy@gmail.com>
2025-07-17 13:55:29 +0900

    [0717] 연합 배포 세션 - PWA 실습

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // 새 버전 생기면 자동 업데이트
+      includeAssets: [
+        'favicons/favicon.ico',
+        'favicons/apple-touch-icon.png',
+        'favicons/favicon-96x96.png',
+        'favicons/favicon.svg'
+      ],
+      manifest: {
+        name: '2025 연합 세션',
+        short_name: '연합 세션',
+        description: '2025 연합 세션 PWA 앱',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'favicons/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'favicons/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: { port: 3000 },
   preview: { port: 3000 },
   build: { outDir: 'build' },
